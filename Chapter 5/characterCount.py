7163dc180666d5d9a59af9f807d3d2fce81c19c5
Brian Wisniewski <minidgenius@gmail.com>
2021-04-08 19:31:52 -0400

    Wrote characterCount.py and pprint

---
+++ Chapter 5/characterCount.py
@@ -0,0 +1,8 @@
+message = 'It was the best of times, it was the worst of times.'
+count = {}
+
+for character in message:
+    count.setdefault(character, 0)
+    count[character] = count[character]+1
+
+print(count)
---
+++ Chapter 5/prettyCharacterCount.py
@@ -0,0 +1,10 @@
+import pprint
+
+message = 'It was the best of times, it was the worst of times.'
+count = {}
+
+for character in message:
+    count.setdefault(character, 0)
+    count[character] = count[character]+1
+
+pprint.pp rint(count)
