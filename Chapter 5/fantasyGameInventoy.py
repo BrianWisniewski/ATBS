d3895fda61bfd330bf2215ab62edfe2d7516b91d
Brian Wisniewski <56554110+BrianWisniewski@users.noreply.github.com>
2021-06-27 21:41:33 -0400

    Added way to add loot in list form to inventory

--- Chapter 5/fantasyGameInventoy.py
+++ Chapter 5/fantasyGameInventoy.py
@@ -1,14 +1,26 @@
 
+def addToInventory(inventory, addedItems):
+
+    for item in addedItems:
+        if item in inventory.keys():
+            inventory[item] += 1
+        else:
+            inventory[item] = 1
+
+    return inventory
+
 def displayInventory(inventory):
     total = 0
     print('Inventory:')
-    for item, num in inventory.items:
+    for item, num in inventory.items():
         print(num, item)
         total += num
     print(f'Total number of items: {total}')
 
 if __name__ == '__main__':
-    print('Enter the inventory in dictionary format:')
-    inv = eval(input())
 
-    displayInventory(inv)
+    inv = {'gold coin': 42, 'rope': 1}
+    loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
+    newInv = addToInventory(inv, loot)
+    displayInventory(newInv)
